4a9dbc95e14844afac6904b2d464fa1c03635b38
mehmet <tas349873@gmail.com>
2022-09-28 21:00:54 +0300

    update

---
+++ users/serializers.py
@@ -0,0 +1,37 @@
+from asyncore import write
+from wsgiref.validate import validator
+from rest_framework import serializers, validators
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[validators.UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={"input_type": "password"}
+
+    )
+
+    password1 = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={"input_type": "password"}
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            'password1'
+        )
