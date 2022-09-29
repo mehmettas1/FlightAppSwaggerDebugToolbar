580f884ec271ee5858da5b606e24f831ad0749b4
mehmet <tas349873@gmail.com>
2022-09-29 21:53:21 +0300

    update

--- flight/serializers.py
+++ flight/serializers.py
@@ -16,9 +16,26 @@ class FlightSerializer(serializers.ModelSerializer):
             "etd"
         )
         
+class PassengerSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Passenger
+        fields = "__all__"
 
 class ReservationSerializer(serializers.ModelSerializer):
     
+    passenger = PassengerSerializer(many=True, required=True)
+    flight = serializers.StringRelatedField()     # default read_only=True
+    flight_id = serializers.IntegerField(write_only=True)
+    user = serializers.StringRelatedField()     # default read_only=True
+    user_id = serializers.IntegerField(write_only=True)
     class Meta:
         model = Reservation
-        fields = "__all__"
+        fields = (
+            "id",
+            "flight",  # GET
+            "flight_id",  # POST
+            "user",  # GET
+            "user_id",  # POST
+            "passenger"
+        )
