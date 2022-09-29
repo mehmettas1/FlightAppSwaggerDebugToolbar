9bfd9f1667e9bb4d26f94edbde7d4241e5334336
mehmet <tas349873@gmail.com>
2022-09-29 10:31:42 +0300

    update

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,12 @@
-from django.conf import settings
 from rest_framework import serializers, validators
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+# from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from dj_rest_auth.serializers import TokenSerializer
+
+
+User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -33,4 +38,38 @@ class RegisterSerializer(serializers.ModelSerializer):
             'last_name',
             'password',
             'password1'
+        )
+
+    def validate(self, data):
+        if data['password'] != data['password1']:
+            raise serializers.ValidationError(
+                {"password": "Password didn't match..... "}
+            )
+        return data
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        validated_data.pop('password1')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email'
+        )
+
+
+class CustomTokenSerializer(TokenSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta(TokenSerializer.Meta):
+        fields = (
+            'key',
+            'user'
         )
