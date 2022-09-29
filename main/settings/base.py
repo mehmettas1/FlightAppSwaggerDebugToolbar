40fcfbe7af5762f61edc0d180f08423703fb621b
mehmet <tas349873@gmail.com>
2022-09-29 12:14:11 +0300

    update

--- main/settings/base.py
+++ main/settings/base.py
@@ -135,3 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+REST_AUTH_SERIALIZERS = {
+    'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
+}
