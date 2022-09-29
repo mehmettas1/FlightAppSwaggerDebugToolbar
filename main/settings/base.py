bafa4406ca432ca27d192dda4279bb6d52a488a2
mehmet <tas349873@gmail.com>
2022-09-29 20:20:32 +0300

    update

--- main/settings/base.py
+++ main/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flight',
     # Third party apps: 
         'drf_yasg',
     # 'debug_toolbar',
